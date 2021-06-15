80ac35d43003a635904b268042a50d8cec0185af
Rishabhrock <rishaabhkmr@gmail.com>
2021-06-15 14:53:44 +0530

    Added Interface Layer

--- src/main/java/com/sapient/vaccinedrive/service/EmployeeDetails.java
+++ src/main/java/com/sapient/vaccinedrive/service/EmployeeDetails.java
@@ -5,7 +5,8 @@ import com.sapient.vaccinedrive.model.EmployeeRecord;
 import java.util.List;
 
 public interface EmployeeDetails {
-//    public String insertRecord(List<EmployeeRecord> records);
-//    public String deleteRecord(List<EmployeeRecord> records);
-//    public String EditRecord(List<EmployeeRecord> records);
+    public String insertRecord(List<EmployeeRecord> records);
+    public String deleteRecord(List<EmployeeRecord> records);
+    public String getRecord();
+    public String EditRecord(List<EmployeeRecord> records);
 }
